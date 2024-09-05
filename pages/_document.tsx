37061d079dcac5ef7af0e22c138fa54a1275108d
Jaspreet Bawa <87329149+jassBawa@users.noreply.github.com>
2024-09-05 18:54:21 +0530

    fix: type declaration Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+// Type Declation For TSX
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "widget-web-component": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        projectid: string;
+      };
+    }
+  }
+}
+
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -10,10 +26,10 @@ export default function Document() {
         <NextScript />
 
 {/*           // <div style={{ position: "fixed", bottom: "50px", right: "20px" }}> */}
-    <widget-web-component projectid="cm0p8ycs90001dilpl9aogcop"></widget-web-component>
+        <widget-web-component projectid="cm0p8ycs90001dilpl9aogcop"></widget-web-component>
 {/*   // </div> */}
 
-  <script async src="https://opinify-widget-w24d.vercel.app/widget.umd.js"></script>
+      <script async src="https://opinify-widget-w24d.vercel.app/widget.umd.js"></script>
       </body>
     </Html>
   )
