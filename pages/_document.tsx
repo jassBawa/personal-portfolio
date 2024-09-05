c2772a5d427948e206d45059cea1ca06fd3ee703
Jaspreet Bawa <87329149+jassBawa@users.noreply.github.com>
2024-09-05 17:43:37 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,15 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        
         <Main />
         <NextScript />
+
+          // <div style={{ position: "fixed", bottom: "50px", right: "20px" }}>
+    <widget-web-component projectid="cm0p8ycs90001dilpl9aogcop"></widget-web-component>
+  // </div>
+
+  <script async src="https://opinify-widget-w24d.vercel.app/widget.umd.js"></script>
       </body>
     </Html>
   )
