efe0af56c0561c5e66a049343d0f3e1732f0cfb8
Jaspreet Bawa <87329149+jassBawa@users.noreply.github.com>
2024-09-05 18:57:37 +0530

    fix: styling of the component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,9 +25,9 @@ export default function Document() {
         <Main />
         <NextScript />
 
-{/*           // <div style={{ position: "fixed", bottom: "50px", right: "20px" }}> */}
-        <widget-web-component projectid="cm0p8ycs90001dilpl9aogcop"></widget-web-component>
-{/*   // </div> */}
+        <div style={{ position: "fixed", bottom: "50px", right: "20px" }}>
+          <widget-web-component projectid="cm0p8ycs90001dilpl9aogcop"></widget-web-component>
+        </div>
 
       <script async src="https://opinify-widget-w24d.vercel.app/widget.umd.js"></script>
       </body>
